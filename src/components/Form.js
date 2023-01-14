6f37d6a7a2aa1ff3624cc5d89721e785a2638ddc
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-14 13:33:44 +0300

    Followed along the react hooks forms Controlled COmponents

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+import { Fragment } from "react";
+
+function Form(props) {
+  
 
   return (
+    <Fragment>
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={props.handleFirstNameChange} value={props.firstName} />
+      <input type="text" onChange={props.handleLastNameChange} value={props.lastName} />
       <button type="submit">Submit</button>
     </form>
+    <form>
+        <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+        <input
+          type="checkbox"
+          id="newsletter"
+          onChange={props.handleNewsletterChange}
+          
+          checked={props.newsletter}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </Fragment>
   );
 }
 
